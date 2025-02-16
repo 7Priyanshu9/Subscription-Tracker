37bd9417f04e43783f346a207db925a84cf9dc33
7Priyanshu9 <priyanshupaul32@gmail.com>
2025-02-16 20:22:51 +0530

    made some flow control

--- middleware/arcjet.middleware.js
+++ middleware/arcjet.middleware.js
@@ -5,6 +5,7 @@ const arcJrtMiddleware = async (req, res, next) => {
     try {
         const decision = await aj.protect(req);
 
+        
 
         if (decision.isDenied()) {
             if (decision.reason.isRateLimit())
